6be098249759bc52eb7e86dda0b20345e89e4054
Boateng Jehoshaphat Osei <jehoosei5@gmail.com>
2026-09-10 21:26:07 +0000

    frontend fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,23 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+
+import RequireAuth from './components/RequireAuth'
+import DocumentsPage from './pages/DocumentsPage'
 import LoginPage from './pages/LoginPage'
+import UploadPage from './pages/UploadPage'
 
 function App() {
-  return <LoginPage />
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+
+      <Route element={<RequireAuth />}>
+        <Route path="/documents" element={<DocumentsPage />} />
+        <Route path="/documents/upload" element={<UploadPage />} />
+      </Route>
+
+      <Route path="*" element={<Navigate to="/documents" replace />} />
+    </Routes>
+  )
 }
 
 export default App
